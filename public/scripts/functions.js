9843455aeaf4c41c5a82da6342b6e82d26fdbd3f
steveyongcc <steveyongcc@hotmail.com>
2018-05-19 12:42:07 +0000

    so

--- public/scripts/functions.js
+++ public/scripts/functions.js
@@ -1,7 +1,7 @@
 function submitComment(commentControl) {
     var comments = document.getElementsByName(commentControl)[0].value;
-    var subscriptionKey = "TEXT_ANALYTICS_API_KEY";
-    var url = "TEXT_ANALYTICS_ENDPOINT/sentiment";
+    var subscriptionKey = "1e930b7bff9b4785a804dc0260d9a581";
+    var url = "https://westcentralus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment";
 
     var payload = '{ "documents": [ { "language": "en-US", "id": "1", "text": "' + comments + '" }]}';
 
